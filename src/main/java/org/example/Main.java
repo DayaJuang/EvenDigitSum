069408ff1034aab478090db751e70edf6a01edf5
daya <dayajuangmahaputra@gmail.com>
2022-11-11 12:01:57 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,31 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ans = getEvenDigitSum(123456789);
+        System.out.println(ans);
+    }
+
+    public static int getEvenDigitSum(int n){
+        // Check if n is positive or not
+        if(n < 0)
+            return -1;
+
+        // Calculate the sum of even number
+        int sum = 0;
+        int digit;
+
+        while(n > 0){
+            digit = n % 10; // take the digit of n
+
+            //Check if the digit is even number or not
+            if(digit % 2 == 0){
+                // sum the number
+                sum += digit;
+            }
+            n /= 10; // Reduce the digit number of n
+        }
+
+        return sum;
     }
 }
